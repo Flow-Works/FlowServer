4975bb13ce93b03af213cda592e7c13b0c981a50
ThinLiquid <thnlqd@gmail.com>
2024-01-17 20:03:49 +0000

    feat: changed `/apps/list` to a list of URLs
    BREAKING CHANGE: removes the need for `init.js` as the `/apps/list` returns a list of URLs

--- src/apps.js
+++ src/apps.js
@@ -1,40 +1,10 @@
 const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const { slowDown } = require('express-slow-down')
+const config = require('../config.json')
 
 const app = express.Router()
 
-const limiter = slowDown({
-  windowMs: 15 * 60 * 1000,
-  delayAfter: 10,
-  delayMs: (hits) => hits * 100
-})
-
-function readJSONFiles (folderPath, callback) {
-  fs.readdir(folderPath, (err, files) => {
-    if (err) throw new Error('Error reading folder:', err)
-
-    const jsonFiles = files.filter(file => path.extname(file) === '.json')
-    const jsonObjects = []
-
-    jsonFiles.forEach(file => {
-      const filePath = path.join(folderPath, file)
-      try {
-        const data = fs.readFileSync(filePath, 'utf8')
-        const jsonObject = JSON.parse(data)
-        jsonObjects.push(jsonObject)
-      } catch (parseErr) {
-        console.error(`Error parsing JSON in file ${file}:`, parseErr)
-      }
-    })
-
-    callback(jsonObjects)
-  })
-}
-
-app.get('/list', limiter, (req, res) => {
-  readJSONFiles(path.join(__dirname, '../repos'), (list) => { res.send(list) })
+app.get('/list', (req, res) => {
+  res.send(config.repos)
 })
 
 module.exports = app
